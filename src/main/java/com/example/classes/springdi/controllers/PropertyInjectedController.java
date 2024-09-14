0d262a801b72557a68ae317f9cfb9938ec915c9e
David <dbcdavid.clk@gmail.com>
2024-09-14 18:59:46 -0300

    Added qualifiers and names to services to define which bean should be injected for each dependency

--- src/main/java/com/example/classes/springdi/controllers/PropertyInjectedController.java
+++ src/main/java/com/example/classes/springdi/controllers/PropertyInjectedController.java
@@ -2,11 +2,13 @@ package com.example.classes.springdi.controllers;
 
 import com.example.classes.springdi.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
+    @Qualifier("propertyGreetingService")
     @Autowired
     GreetingService greetingService;
 
--- src/main/java/com/example/classes/springdi/controllers/SetterInjectedController.java
+++ src/main/java/com/example/classes/springdi/controllers/SetterInjectedController.java
@@ -2,6 +2,7 @@ package com.example.classes.springdi.controllers;
 
 import com.example.classes.springdi.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -9,6 +10,7 @@ public class SetterInjectedController {
 
     private GreetingService greetingService;
 
+    @Qualifier("setterGreetingBean")
     @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
