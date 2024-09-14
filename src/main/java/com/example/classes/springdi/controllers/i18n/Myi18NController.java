cf7471714d73e094cff58f22d0ea114ff2e6b8bc
David <dbcdavid.clk@gmail.com>
2024-09-14 19:14:56 -0300

    Added controller and services for greetings in EN and ES

---
+++ src/main/java/com/example/classes/springdi/controllers/i18n/Myi18NController.java
@@ -0,0 +1,20 @@
+package com.example.classes.springdi.controllers.i18n;
+
+import com.example.classes.springdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Myi18NController {
+
+    private final GreetingService greetingService;
+
+    public Myi18NController(@Qualifier("i18NService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+
+}
---
+++ src/main/java/com/example/classes/springdi/services/EnglishGreetingService.java
@@ -0,0 +1,13 @@
+package com.example.classes.springdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("EN")
+@Service("i18NService")
+public class EnglishGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World - EN";
+    }
+}
